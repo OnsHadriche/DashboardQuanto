0a2f7131777d7761b9ca6447ea15d70013e234e2
ons <ounshadriche@gmail.com>
2024-07-16 18:10:07 +0300

    Add default layout

---
+++ src/routes/DefaultRoutes.jsx
@@ -0,0 +1,11 @@
+// project imports
+import Navbar from 'layout/DefaultLayout/NavBar';
+
+// ==============================|| AUTHENTICATION ROUTING ||============================== //
+
+const DefaultRoutes = {
+  path: '/',
+  element: <Navbar />
+};
+
+export default DefaultRoutes;
