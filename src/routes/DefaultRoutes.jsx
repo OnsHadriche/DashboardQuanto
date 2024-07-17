458a32b4a80b893d91657fc3a5878fcad298920c
ons <ounshadriche@gmail.com>
2024-07-17 17:21:32 +0300

    Create defaultRoute

--- src/routes/DefaultRoutes.jsx
+++ src/routes/DefaultRoutes.jsx
@@ -1,11 +1,40 @@
 // project imports
-import Navbar from 'layout/DefaultLayout/NavBar';
+import MainContent from 'layout/DefaultLayout';
+import { lazy } from 'react';
+import Loadable from 'ui-component/Loadable';
+//home routing
+const Home = Loadable(lazy(() => import('views/defaultApp/Home')));
+const HowToUse = Loadable(lazy(() => import('views/defaultApp/HowToUse')));
+const Contact = Loadable(lazy(() => import('views/defaultApp/Contact')));
 
 // ==============================|| AUTHENTICATION ROUTING ||============================== //
 
 const DefaultRoutes = {
-  path: '/',
-  element: <Navbar />
+    path: '/',
+    element: <MainContent />,
+    children: [
+        {
+            path: '/',
+            element: <Home />
+        },
+        {
+            path: 'home',
+            children: [
+                {
+                    path: 'default',
+                    element: <Home />
+                },
+                {
+                    path: 'howToUse',
+                    element: <HowToUse />
+                },
+                {
+                    path: 'contact',
+                    element: <Contact />
+                }
+            ]
+        }
+    ]
 };
 
 export default DefaultRoutes;
